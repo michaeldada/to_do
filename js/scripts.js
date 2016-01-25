7de61af40b6df62777b7efa3209817a98ce45c2b
mary warrington and paul hess <marywarrington@gmail.com>
2016-01-25 11:49:49 -0800

    Add passing specs to add/show one list item

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
+function ToDo(list) {
+  this.list = [];
+}
 
+ToDo.prototype.addItem = function(input1) {
+  this.list.push(input1);
+  return this.list;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,1 +1,19 @@
+describe('ToDo', function() {
+  it("should create an object with a list of things to do", function() {
+    var testToDo = new ToDo();
+    expect(testToDo.list).to.eql([]);
+  });
 
+  it('should add list items to the list', function() {
+    var testToDo = new ToDo();
+    expect(testToDo.addItem("shower")).to.eql(['shower']);
+  });
+  it('should display the list items in the list when added', function() {
+    var testToDo = new ToDo();
+    expect(testToDo.addItem("shower")).to.eql(['shower']);
+  });
+  // it('should display multiple list items in the list when added', function() {
+  //   var testToDo = new ToDo();
+  //   expect(testToDo.addItem("shower", "clean", "eat")).to.eql(['shower', 'clean', 'eat']);
+  // });
+});
